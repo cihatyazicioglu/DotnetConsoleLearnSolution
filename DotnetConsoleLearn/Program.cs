717d1df55c9628a545fbe87a1e96485bc9c0cf2b
RAVEN <RAVEN@DESKTOP-Q41U2OJ>
2022-12-24 23:42:26 +0300

    Proje dosyası ekle.

---
+++ DotnetConsoleLearn/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Results Of Exam !");
+
+string name1 = "Kylian Mbappe";
+int examresult1 = 50 ;
+Console.WriteLine(name1);
+Console.WriteLine(examresult1);
+
+if (examresult1 >= 50)
+
+    Console.WriteLine("Mission Passed");
+
+
+else 
+
+    Console.WriteLine("Mission Failed");
+
+
+string name2 = "Lionel Messi";
+int examresult2 = 49;
+Console.WriteLine(name2);   
+Console.WriteLine(examresult2);
+
+if (examresult2 >= 50)
+    Console.WriteLine("Mission Passed");
+
+else
+
+    Console.WriteLine("Mission Failed");
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
